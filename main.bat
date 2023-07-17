921811364852181fc0f9e04911ef687f5f6b54f6
jg <57115019+joeyagreco@users.noreply.github.com>
2023-07-16 20:55:22 -0500

    stuff

--- main.bat
+++ main.bat
@@ -2,6 +2,8 @@
 
 IF "%1"=="fmt" (
     call scripts\batch\format.bat
+) ELSE IF "%1" == "test" (
+    call scripts\batch\test.bat
 ) ELSE (
     echo Invalid flag. Please specify a valid flag.
 )
--- scripts/batch/format.bat
+++ scripts/batch/format.bat
@@ -1,4 +1,5 @@
 @echo off
 
+python scripts\batch\helper\duplicate-test-methods.py
 black --config=pyproject.toml .
 autoflake --config=pyproject.toml .
